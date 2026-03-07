fa6389fc0d9c1da63ea9c42272e0454f9b1a3dd0
Jordan Kusuda <kusuda.jordan@gmail.com>
2026-03-07 15:18:51 -0500

    Login/Signup Implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Navbar } from './components'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
+import { AuthProvider } from './contexts/AuthContext'
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col min-h-screen bg-white text-black">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="flex flex-col min-h-screen bg-white text-black">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
