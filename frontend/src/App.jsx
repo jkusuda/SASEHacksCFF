b91e349e28bf3d73437c3485fded2d3dd9aa8815
Jordan Kusuda <kusuda.jordan@gmail.com>
2026-03-07 19:11:04 -0500

    Extension integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { HashRouter, Routes, Route, useLocation } from 'react-router-dom'
 import { Navbar } from './components'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
-import Popup from './pages/Popup'
 import Dashboard from './pages/Dashboard'
 import VirtualCards from './pages/VirtualCards'
 import { AuthProvider } from './contexts/AuthContext'
@@ -12,18 +11,16 @@ function AppLayout() {
   const location = useLocation()
   const isDashboard = location.pathname.startsWith('/dashboard')
   const isLogin = location.pathname === '/login'
-  const isPopup = location.pathname === '/popup'
 
   return (
     <div className="flex flex-col min-h-screen bg-white text-black">
-      {!isDashboard && !isLogin && !isPopup && <Navbar />}
+      {!isDashboard && !isLogin && <Navbar />}
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard/virtual-cards" element={<VirtualCards />} />
         <Route path="/dashboard/settings" element={<Dashboard />} />
-        <Route path="/popup" element={<Popup />} />
       </Routes>
     </div>
   )
